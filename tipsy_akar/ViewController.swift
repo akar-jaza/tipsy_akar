d78d41eefabe74e8f727ddd99ce0a34cf0bcbdea
akar-jaza <64991287+akar-jaza@users.noreply.github.com>
2023-03-22 20:00:36 +0300

    Initial Commit

---
+++ tipsy_akar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tipsy_akar
+//
+//  Created by Akar jaza on 3/22/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
