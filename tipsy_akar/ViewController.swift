741f124addfa5ca4c21aa972e45c99f516176a93
akar-jaza <64991287+akar-jaza@users.noreply.github.com>
2023-03-22 20:51:08 +0300

    First Commit

--- tipsy_akar/ViewController.swift
+++ tipsy_akar/ViewController.swift
@@ -8,10 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var billLabel: UILabel = {
+        let billLabel = UILabel()
+        billLabel.text = "Enter Bill Total"
+        billLabel.font = .systemFont(ofSize: 25)
+        billLabel.textColor = .gray
+        billLabel.translatesAutoresizingMaskIntoConstraints = false
+        return billLabel
+    }()
+    
+    var UpStackView: UIStackView = {
+        let sv = UIStackView()
+        sv.translatesAutoresizingMaskIntoConstraints = false
+        return sv
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupUI()
+    }
+    
+    func setupUI() {
+        view.backgroundColor = .blue
+        
+        UpStackView.addArrangedSubview(billLabel)
+        
+        NSLayoutConstraint.activate([
+            UpStackView.topAnchor.constraint(equalTo: view.topAnchor, constant: 10),
+            UpStackView.rightAnchor.constraint(equalTo: view.rightAnchor),
+            UpStackView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            UpStackView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 40)
+        ])
     }
 
 
